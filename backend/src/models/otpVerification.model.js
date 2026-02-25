735a337090f6ce892dd2879d8a7250794848dc79
Kumari G <151625325+kumari01@users.noreply.github.com>
2026-02-25 15:25:04 +0530

    Update otpVerification.model.js

--- backend/src/models/otpVerification.model.js
+++ backend/src/models/otpVerification.model.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 
-const otpSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+const otpSchemaDept = new mongoose.Schema(
+  {
+    // 🔑 OWNER (USER / ADMIN / ANY ROLE)
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+
+    otp: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 6
+    },
+
+    otp_type: {
+      type: String,
+      enum: ["PHONE", "EMAIL", "RESET"],
+      required: true
+    },
+
+    expires_at: {
+      type: Date,
+      required: true
+    }
   },
-
-  otp: {
-    type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 6
-  },
-  otp_type: {
-  type: String,
-  enum: ["PHONE", "EMAIL","RESET"],
-  required: true
-},
-
-  expires_at: {
-    type: Date,
-    required: true
-  }
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 // 🔥 TTL INDEX
-// Automatically delete document when expires_at time passes
-otpSchema.index({ expires_at: 1 }, { expireAfterSeconds: 0 });
+otpSchemaDept.index({ expires_at: 1 }, { expireAfterSeconds: 0 });
 
-
-export default mongoose.model("OTPVerification", otpSchema);
+export default mongoose.model("OTPVerification", otpSchemaDept);
